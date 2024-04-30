5df45698536c931638dfc4a56d1b439a30afb2d7
toshiaki <conamn1185@gmail.com>
2024-04-30 21:10:36 +0900

    fix

--- origin/main.go
+++ origin/main.go
@@ -7,13 +7,22 @@ import (
 
 func main() {
 	// 静的ファイルのディレクトリを設定
-	fs := http.FileServer(http.Dir("./static"))
+	fs := http.FileServer(http.Dir("."))
 	http.Handle("/", fs)
+
+	http.Handle("/404", http.NotFoundHandler())
 
 	// サーバーをポート8080で起動
 	log.Println("Listening on http://localhost:8080...")
-	err := http.ListenAndServe(":8080", nil)
+	err := http.ListenAndServe(":8080", logRequest(http.DefaultServeMux))
 	if err != nil {
 		log.Fatal(err)
 	}
+}
+
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.URL.Path)
+		handler.ServeHTTP(w, r)
+	})
 }
